e0e26318709b7379e3d67b0b32a8dfb6b94ae8b1
Ude <Ude@Ude-PC>
2015-08-24 18:17:47 -0300

    inicio

--- src/projetointegrador3semestre/ProjetoIntegrador3Semestre.java
+++ src/projetointegrador3semestre/ProjetoIntegrador3Semestre.java
@@ -16,7 +16,8 @@ public class ProjetoIntegrador3Semestre {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Projeto");
+        int x = 20;
     }
     
 }
